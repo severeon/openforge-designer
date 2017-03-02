95847cb78f0b6443b06d23af08ad23cd53a76d4e
Thomas Quick <thomas.quick@monsanto.com>
2017-03-01 19:27:27 -0600

    feat: added more tile images

--- app/components/design/tiles/index.js
+++ app/components/design/tiles/index.js
@@ -8,6 +8,15 @@ const tiles = {
     SA: { width: 2, height: 6, codename: 'sa', category: 'floor', color: '#CFC' },
     SB: { width: 2, height: 8, codename: 'sb', category: 'floor', color: '#CFC' }
   },
+  edge: {
+    'E-EDGE-WALL': { width: 4, height: 5, codename: 'e-edge-wall', category: 'edge wall' },
+    'EA-EDGE-WALL': { width: 6, height: 7, codename: 'ea-edge-wall', category: 'edge wall' },
+    'U-EDGE-WALL': { width: 8, height: 9, codename: 'u-edge-wall', category: 'edge wall' },
+    'R-EDGE-WALL': { width: 4, height: 9, codename: 'r-edge-wall', category: 'edge wall', color: '#CFC' },
+    'S-EDGE-WALL': { width: 2, height: 5, codename: 's-edge-wall', category: 'edge wall', color: '#CFC' },
+    'SA-EDGE-WALL': { width: 2, height: 7, codename: 'sa-edge-wall', category: 'edge wall', color: '#CFC' },
+    'SB-EDGE-WALL': { width: 2, height: 9, codename: 'sb-edge-wall', category: 'edge wall', color: '#CFC' }
+  },
   wall: {
     A: { width: 1, height: 4, codename: 'a', category: 'wall', color: '#CCF' },
     BA: { width: 1, height: 3, codename: 'ba', category: 'wall', color: '#CCF' },
